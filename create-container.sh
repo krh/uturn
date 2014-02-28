d2db1062decb6c6d76c0d3c13da5e85b421cb333
U. Artie Eoff <ullysses.a.eoff@intel.com>
2014-02-28 10:23:28 -0800

    create-container: support 32-bit creation on 64-bit OS
    Use arch and basearch vars to determine the types
    of rpms to install with yum.  Name the local repo download
    and create a local yum repo configuration file accordingly.
    
    Add base-i686-container.sh config for creating 32-bit
    containers.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- create-container.sh
+++ create-container.sh
@@ -14,24 +14,36 @@ path=$(readlink -f $name)
 
 test -d $path || btrfs subvolume create $path
 
+# yum will use the vars from the install root
+mkdir -p $path/etc/yum/vars/
+echo $basearch > $path/etc/yum/vars/basearch
+echo $arch > $path/etc/yum/vars/arch
+
+# create a local yum repo configuration to be used by install
+cat << EOF > local.$repo-$release-$arch.repo
+[local]
+name=Local Repo
+baseurl=file://$PWD/rpms/$repo-$release-$arch
+EOF
+
 case $1 in
     download)
 	yumdownloader -y --releasever=$release --installroot=$path	\
-	    --destdir=$PWD/rpms/fedora-20-x86_64			\
+	    --destdir=$PWD/rpms/$repo-$release-$arch			\
 	    --disablerepo='*'						\
 	    --enablerepo=fedora						\
 	    --enablerepo=updates					\
 	    --resolve							\
 	    $rpms
 
-	createrepo -o rpms/fedora-20-x86_64 rpms/fedora-20-x86_64
+	createrepo -o rpms/$repo-$release-$arch rpms/$repo-$release-$arch
 	;;
 
     install)
 	yum -y --releasever=$release --nogpg --installroot=$path	\
-	    --downloaddir=$PWD/rpms/fedora-20-x86_64			\
+	    --downloaddir=$PWD/rpms/$repo-$release-$arch		\
 	    --disablerepo='*'						\
-	    -c local.repo --enablerepo=local				\
+	    -c local.$repo-$release-$arch.repo --enablerepo=local	\
 	    install $rpms
 
 	cp uturn-builder.service $path/etc/systemd/system
